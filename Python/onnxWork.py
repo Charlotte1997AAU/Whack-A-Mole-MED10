f9fe025a1fa0b37b107456cc88e5b045a1c751bd
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2025-04-04 12:54:25 +0200

    modified model

--- Python/onnxWork.py
+++ Python/onnxWork.py
@@ -2,6 +2,20 @@ import onnx
 from onnx import numpy_helper
 
 model = onnx.load("SGD_Model.onnx")
+nodesToRemove = []
+for node in model.graph.node:
+    if node.op_type == "ZipMap":
+        nodesToRemove.append(node)
+
+for node in model.graph.node:
+    if node.op_type == "ArrayFeatureExtractor":
+        nodesToRemove.append(node)
+
+for output in model.graph.output:
+    if output.name == "output_label":
+        output.name = "final_label"
+
+onnx.save(model, "modified_sgd_model.onnx")
 
 for output in model.graph.output:
     print(output.name)
