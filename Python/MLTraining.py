e9d1b57a096224ddfd505e44ac09b3a3e8de1fd6
kristian1402 <ksaren20@student.aau.dk>
2025-04-23 12:26:09 +0200

    working on pca

--- Python/MLTraining.py
+++ Python/MLTraining.py
@@ -53,3 +53,32 @@ def trainModel(data, model):
 
     plt.show()
     """
+
+def trainPCA(data, model):
+    # Load the dataset
+    df = data
+    model_name = type(model).__name__
+
+
+    X = df.drop(columns=['GoalGesture'])  # Features
+    y = df['GoalGesture']  # Target
+
+    # Split the dataset into training and testing (with stratification for balanced classes)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    model.fit(X_train, y_train)
+
+    # Evaluate the best model on the test set
+    y_pred_best_model = model.predict(X_test)
+
+    # Calculate accuracy and print confusion matrix for the best model
+    model_accuracy_best = accuracy_score(y_test, y_pred_best_model)
+    model_cm = confusion_matrix(y_test, y_pred_best_model)
+    print(f"Best accuracy for {model_name}: {model_accuracy_best:.2f}")
+    print("Confusion Matrix for Best Model:")
+    print(model_cm)
+    print("Classification Report for Best Model:")
+    print(classification_report(y_test, y_pred_best_model))
+
+    joblib.dump(model, f"{model_name}PCA_L.pkl")
+    print(f"Trained and saved {model_name}")
--- Python/dimensionality.py
+++ Python/dimensionality.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import seaborn as sns
+import MLTraining
 
 
 def lda():
@@ -22,7 +24,8 @@ def lda():
     plt.show()
 
 
-def PCA_visualization():
+
+def PCAcalc():
     data = pd.read_csv("test Data set/TrainingSetWdeltas_L.csv")
     data = data[data['activeCube'] != -1]
     X = data.drop(columns=["GoalGesture"])
@@ -30,17 +33,31 @@ def PCA_visualization():
 
     X_scaled = StandardScaler().fit_transform(X)
 
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=0.95)
     X_pca = pca.fit_transform(X_scaled)
+    lda = LinearDiscriminantAnalysis(n_components=None)
+    X_lda = lda.fit_transform(X_pca, y)
+    plt.scatter(X_lda[:, 0], X_lda[:, 1], X_lda[:, 2], c=y, cmap='rainbow', edgecolor='k', alpha=0.7)
+    plt.xlabel("LDA Component 1")
+    plt.ylabel("LDA Component 2")
+    plt.title("LDA projection of the dataset")
+    plt.show()
 
+    data = pd.DataFrame(X_pca)
+    data["GoalGesture"] = y
+    return data
+
+
+def PCAVisual(X_pca, y):
     pca_df = pd.DataFrame(X_pca, columns=["PC1", "PC2"])
     pca_df["GoalGesture"] = y
-
     plt.figure(figsize=(8, 6))
     sns.scatterplot(data=pca_df, x="PC1", y="PC2", hue="GoalGesture", palette="Set2")
     plt.title("PCA: Projection of Gestures")
     plt.show()
 
-    print("Explained Variance Ratio:", pca.explained_variance_ratio_)
 
-PCA_visualization()
+data = PCAcalc()
+
+#MLTraining.trainPCA(data, RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1, max_depth=10, min_samples_split=2))
+
