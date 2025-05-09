e0c585d98f0d6783e3b08c6fb116f4fad40f6d4a
ha <mathias.halilovic94@gmail.com>
2025-05-09 12:39:57 +0200

    tests

--- Python/emgServer.py
+++ Python/emgServer.py
@@ -11,7 +11,7 @@ import featureSelection
 from sklearn.preprocessing import StandardScaler
 from scipy.special import softmax
 
-participantNr = 14
+participantNr = 15
 
 WINDOW_SIZE = 40
 NUM_CHANNELS = 11
